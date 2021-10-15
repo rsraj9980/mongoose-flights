35f3f7513694eec4181431faae6dbb24ab636672
rsraj9980 <91348564+rsraj9980@users.noreply.github.com>
2021-10-15 16:38:10 -0400

    MVP version

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -11,21 +11,9 @@ function create(req, res) {
     const ticket = new Ticket(req.body);
     ticket.flight = req.params.id;
     ticket.save(function(err) {
-        res.redirect(`/flights/${flight._id}`);
+        res.redirect(`/flights/${req.params.id}`);
     });
 }
-
-
-// function create(req, res) {
-//     Flight.findById(req.params.id, function(err, flight) {
-//         Ticket.find({ flight: req.params.id }, function(err, tickets) {
-//             res.redirect(`/flights/${req.params.id}`, {
-//                 seat,
-//                 price
-//             });
-//         });
-//     });
-// }
 
 function newTicket(req, res) {
     res.render('tickets/new', { flightId: req.params.id });
